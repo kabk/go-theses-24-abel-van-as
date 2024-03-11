2c2900da2ab47360215bb7e40516050a77d33cc7
Abel <3260305@kabk.nl>
2024-03-11 10:37:35 +0100

    museum website!

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-function toggleText(id) {
+// Functie om tekst te tonen/verbergen
+function toggleText(id, text) {
   var demoElement = document.getElementById(id);
   if (demoElement.innerHTML !== "") {
-    // Als de tekst al is ingesteld, verwijder deze dan
-    demoElement.innerHTML = "";
+      // Als de tekst al is ingesteld, verwijder deze dan
+      demoElement.innerHTML = "";
   } else {
-    // Als de tekst nog niet is ingesteld, stel deze dan in
-    var text = id.replace("demo", "");
-    demoElement.innerHTML = text;
+      // Als de tekst nog niet is ingesteld, stel deze dan in
+      demoElement.innerHTML = text;
   }
 }
+
+window.addEventListener('scroll', function() {
+  const headings = document.querySelectorAll('h3[id]');
+
+  headings.forEach((currentHeading, index) => {
+    const nextHeading = headings[index + 1];
+    
+    if (nextHeading || index === headings.length - 1) { // Controleer of het de laatste <h3> is
+      const currentRect = currentHeading.getBoundingClientRect();
+      const nextRect = nextHeading ? nextHeading.getBoundingClientRect() : null;
+
+      if (nextRect && nextRect.top <= 0) {
+        currentHeading.style.position = 'static';
+      } else {
+        currentHeading.style.position = 'sticky';
+        currentHeading.style.top = 0;
+      }
+    }
+  });
+});
+
